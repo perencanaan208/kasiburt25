77e642bef31b55eb4fde5721518c81e855f95fb7
perencanaan208 <perencanaan208@gmail.com>
2026-08-20 18:41:56 +0700

    Update GAS_URL to include full URL

--- config.js
+++ config.js
@@ -8,4 +8,4 @@
 // Contoh:
 // const GAS_URL = "https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/exec";
 
-const GAS_URL = "AKfycbzUw4wgeopnzqz537XreVsB1tWLpF7VyNd4fSnOng4a49yd6V5ost1rfbdOqjgoM88kxQ/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzUw4wgeopnzqz537XreVsB1tWLpF7VyNd4fSnOng4a49yd6V5ost1rfbdOqjgoM88kxQ/exec";
